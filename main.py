1d1d139ee07d8464acf31732040ed0ff18281aa9
roopasreeap <roopasreeap@gmail.com>
2023-06-09 12:55:01 +0530

    Imported os module,Changes made in position and size  of about button and entry,Added a user guide button

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 ###########################################################################
-#    Maths Tutor
+#    Maths-Tutor
 #
-#    Copyright (C) 2022-2023 Roopasree A P <roopasreeap@gmail.com>
-#    Copyright (C) 2022-2023 Roopasree A P <roopasreeap@gmail.com>
-#    
+#    Copyright (C) 2022-2023 Roopasree A P <roopasreeap@gmail.com>    
 #    
 #    This project is Supervised by Zendalona(2022-2023)
 #
@@ -28,39 +26,50 @@ import speechd
 gi.require_version("Gtk", "3.0")
 gi.require_version('Gst', '1.0')
 from gi.repository import Gtk, Gdk, GObject, GLib,Pango
+
 # Import GST
 from gi.repository import Gst
 import re
-
+import os
 import threading
 
 class MyWindow(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Label and Text Field Example")
+        Gtk.Window.__init__(self, title="Maths-Tutor")
         self.set_border_width(10)
+        
         #Create a vertical box layout
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        
         # Add the VBox container to the main window
         self.add(vbox)
         
         welcome_message = "Welcome to maths tutor!\nPress enter to start "
         
+        
         # create  a Gtk label
         self.label = Gtk.Label()
+        
         # Set the text of the label to the value of welcome_message
         self.label.set_text(welcome_message)
+        
         # Modify the font of the label
         self.modify_font(Pango.FontDescription("Sans 40"))
         
         
+        
         # Define the font color
         font_color = "#0603f0"
+        
         # Define the background color
         background_color = "#ffffff"
+        
         # Set the font color
         self.modify_fg(Gtk.StateFlags.NORMAL, Gdk.color_parse(font_color))
+        
         # Set the background color
         self.modify_bg(Gtk.StateFlags.NORMAL, Gdk.color_parse(background_color))
+        
         # Add the label to the vbox container
         vbox.pack_start(self.label, True, True, 0)
         
@@ -68,35 +77,51 @@ class MyWindow(Gtk.Window):
         #Create a horizontal box layout 
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
 
-        #fix1 = Gtk.Fixed()
-        #hbox.pack_start(fix1, True, True, 0)
-        # Add the hbox to the vbox container
+        fix1 = Gtk.Fixed()
+        hbox.pack_start(fix1, True, True, 0)
+        
+        #Add the hbox to the vbox container
         vbox.pack_start(hbox, False, False, 0)
 
        
         #Create a Gtk.Entry widget and assign it to self.entry
         self.entry = Gtk.Entry()
+        
         # Connect the "activate" signal of the entry widget to the self.on_entry_activated method
         self.entry.connect("activate", self.on_entry_activated)
+        
         #Add entry to horizontal box
         hbox.pack_start(self.entry, False, False, 0)
 
-        #fix2 = Gtk.Fixed()
-        #hbox.pack_start(fix2, True, True, 0)
+        fix2 = Gtk.Fixed()
+        hbox.pack_start(fix2, True, True, 0)
         
-        #hbox.set_hexpand(False)
+        self.cwd = os.getcwd()
 
         
         #Create multiple instances of GtkImage and add them to the vertical box
-        self.image = Gtk.Image.new_from_file("/home/roopasree/Desktop/Game/Image/positive4.png")
+        self.image = Gtk.Image()
+        self.set_image("positive4.png")
         vbox.pack_start(self.image, True, True, 0)
         
+        #Horizontal box for About and User-Guide
+        hbox2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
         
-        #Create a button
-        about_button=Gtk.Button(label="About")
-        about_button.connect("clicked",self.on_about_clicked)
+        #Create user guide button
+        about_button=Gtk.Button(label="User-Guide")
+        about_button.connect("clicked",self.on_user_guide_clicked)
         about_button.set_size_request(1,1)
-        hbox.pack_start(about_button, True, True, 0)
+        hbox2.pack_start(about_button, True, True, 0)
+
+
+        
+        #Create about button
+        about_button=Gtk.Button(label="About")
+        about_button.connect("clicked",self.show_about_dialog)
+        about_button.set_size_request(1,1)
+        hbox2.pack_start(about_button, True, True, 0)
+        
+        vbox.pack_start(hbox2, True, True, 0)
         
         
 
@@ -104,6 +129,7 @@ class MyWindow(Gtk.Window):
 
         self.current_question_index = -1
         self.wrong=False
+        
         # Create a playbin element with the name 'player' and assign it to self.player
         self.player = Gst.ElementFactory.make('playbin', 'player')
         
@@ -119,19 +145,24 @@ class MyWindow(Gtk.Window):
         self.connect('delete-event', self.on_destroy)
         self.connect('destroy', self.on_destroy)
         
+        self.set_question_file(self.cwd+"/calculation.txt")
+
+        
 
     
     def play_file(self, filename):
-        file_path_and_name = 'file:///home/roopasree/Desktop/Game/sounds/'+filename
+        file_path_and_name = 'file:///'+self.cwd+'/sounds/'+filename
         self.player.set_state(Gst.State.READY)
         self.player.set_property('uri',file_path_and_name)
         self.player.set_state(Gst.State.PLAYING)
+
+    def set_image(self, filename):
+	    self.image.set_from_file(self.cwd+"/Image/"+filename);
         
-    def on_read_files(self):
-        filename = "/home/roopasree/project/calculation.txt"
+    def set_question_file(self, file_path):
         self.list = []
 
-        with open(filename, "r") as file:
+        with open(file_path, "r") as file:
             for line in file:
                 stripped_line = line.strip()
                 self.list.append(stripped_line)
@@ -161,27 +192,27 @@ class MyWindow(Gtk.Window):
                 if  time_taken < time_alotted:
                     self.spd_cli.speak("Excellent!")
                     self.label.set_text("Excellent!")
-                    self.image.set_from_file("/home/roopasree/Desktop/Game/Image/positive2.png")
+                    self.set_image("positive2.png")
                     self.play_file('excellent.ogg')
                 elif time_taken < time_alotted+2:
                     self.spd_cli.speak("Very good!")
                     self.label.set_text("Very good!")
-                    self.image.set_from_file("/home/roopasree/Desktop/Game/Image/positive5.png")
+                    self.set_image("positive5.png")
                     self.play_file('very_good.ogg')
                 elif time_taken < time_alotted+4:
                     self.spd_cli.speak("Good!")
                     self.label.set_text("Good!")
-                    self.image.set_from_file("/home/roopasree/Desktop/Game/Image/positive9.png")
+                    self.set_image("positive9.png")
                     self.play_file('next_level_6.ogg')
                 elif time_taken < time_alotted+6:
                     self.spd_cli.speak("Not bad!")
                     self.label.set_text("Not bad!")
-                    self.image.set_from_file("/home/roopasree/Desktop/Game/Image/positive1.png")
+                    self.set_image("positive1.png")
                     self.play_file('ok.ogg')
                 else :
                     self.spd_cli.speak("Okay!")
                     self.label.set_text("Okay!")
-                    self.image.set_from_file("/home/roopasree/Desktop/Game/Image/neg3.png")
+                    self.set_image("neg3.png")
                     self.play_file('try_more_fast.ogg')
 
                 GLib.timeout_add_seconds(3,self.next_question)
@@ -192,7 +223,7 @@ class MyWindow(Gtk.Window):
                 
                 self.label.set_text("sorry,lets try again")
                 self.spd_cli.speak("Sorry!Let's try again")
-                #self.image.set_from_file("/home/roopasree/Desktop/Game/Image/neg5.png")
+                #self.set_image("neg5.png")
                 
                 #time.sleep(3)
                 self.wrong=True
@@ -202,11 +233,14 @@ class MyWindow(Gtk.Window):
             
     def next_question(self):
         self.time_start = time.time()
+        self.entry.grab_focus()
+        
         
         if self.wrong==True:
             self.label.set_text(self.question)
+            
             self.announce_question(self.question, self.make_sound)
-            self.image.set_from_file("/home/roopasree/Desktop/Game/Image/neg5.png")
+            self.set_image("neg5.png")
             self.wrong=False
         else:
             self.current_question_index = self.current_question_index + 1
@@ -221,11 +255,11 @@ class MyWindow(Gtk.Window):
                 threading.Thread(target=self.announce_question,args=[self.question, self.make_sound]).start()
                 
                 self.entry.set_text("")
-                self.image.set_from_file("/home/roopasree/Desktop/Game/Image/neg1.png")
+                self.set_image("neg1.png")
             else:
                 self.spd_cli.speak("Successfully finished!")
                 self.label.set_text("Successfully finished!")
-                self.image.set_from_file("/home/roopasree/Desktop/Game/Image/positive7.png")
+                self.set_image("positive7.png")
                 
                 
     def announce_question(self, question, make_sound):
@@ -246,11 +280,11 @@ class MyWindow(Gtk.Window):
             self.spd_cli.speak(self.convert_signs(self.question))
 
     def on_destroy(self, widget=None, *data):
-        print("CLOS")
+        print("CLOSE")
         self.spd_cli.close()
         
 
-    def show_about_dialog(self):
+    def show_about_dialog(self, button):
         about_dialog = Gtk.AboutDialog()
 
         # Set the relevant properties of the about dialog
@@ -260,7 +294,7 @@ class MyWindow(Gtk.Window):
         
         about_dialog.set_website_label("GNU General Public License,version 0.1\n\n" "Visit MATHS TUTOR Home page")
         
-        about_dialog.set_website("http://wwww,zendalona.com//MATHS TUTOR")
+        about_dialog.set_website("http://wwww,zendalona.com/maths-tutor")
         about_dialog.set_authors(["Roopasree A P"])
         about_dialog.set_documenters(["Roopasree A P"])
         about_dialog.set_artists(["Nalin Sathyan" ,"Dr.Saritha Namboodiri", "Subha I N", "Bhavya P V", "K.Sathyaseelan"])
@@ -268,8 +302,8 @@ class MyWindow(Gtk.Window):
         about_dialog.run()
         about_dialog.destroy()
 
-    def on_about_clicked(self,button):
-         self.show_about_dialog()
+    def on_user_guide_clicked(self,button):
+         print("USER-GUIDE")
 
 
 def main():
@@ -278,7 +312,6 @@ def main():
     win.set_default_size(500,700)
     win.connect("destroy", Gtk.main_quit)
     win.show_all()
-    win.on_read_files()
     Gtk.main()
 
 
